792086c8e631fdb2008492ed268288a3050d8e36
RajabaliR4 <Rajabalir4@gmail.com>
2019-07-01 22:41:42 +0530

    Telegram msg notifcation added

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,36 @@
 var angle = 115;
 $(document).ready(function() {
+	
+	$(".sent").hide();
 
 	setInterval(updateGradient,100);
 	
 	$('ul li a').click(function(event) {
 		$('ul li a').css('opacity', '0.6');
 		$(this).css('opacity', '1');
+	});
+
+
+	//function to send notification to me
+	$('#mybutton').click(function(event){
+
+		$('form').submit(function(event){
+		event.preventDefault();
+		$('#mybutton').hide();
+		var botID = "838585674:AAHc-oeJjg5FjJXI9X6vMbJS219rCtQgfRI";
+		var email = $("input[name='mail']").val()
+		console.log(email);
+		var text = $("textarea").val();
+		console.log(text);
+
+		var msg = "Rajab you have a Message from your website %0AEmail : "+email+" %0AMessage:  "+text;
+		// im gonna add my telegram url here
+		$.get("https://api.telegram.org/bot"+botID+"/sendMessage?chat_id=564221279&text="+msg+"", function(data, status){
+        console.log(status);
+        $(".sent").show('slow/400/fast');
+        $("form").hide('slow/400/fast');
+		});	
+		});
 	});
 
 	$("a[href^='#']").click(function(e) {

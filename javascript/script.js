2aadda5e31b60eaf306263bf6b296660134ad7fe
Rajab <rajabalir4@gmail.com>
2019-07-05 01:24:40 +0530

    Added Projects

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-var angle = 115;
+var angle = 340;
 $(document).ready(function() {
 	
 	$(".sent").hide();
@@ -11,6 +11,23 @@ $(document).ready(function() {
 	});
 
 
+	$(".border").hover(function() {
+		/* Stuff to do when the mouse enters the element */
+		$(this).css({
+			cursor: 'pointer'
+		});
+		$(this).find(".micro").hide();
+		$(this).find(".technologies").css("margin-top","2%");
+		$(this).find(".technologies").show();
+
+	}, function() {
+		/* Stuff to do when the mouse leaves the element */
+		$(this).find(".micro").show();
+		$(this).find(".technologies").hide();
+
+	});
+
+
 	//function to send notification to me
 	$('#mybutton').click(function(event){
 
@@ -19,10 +36,7 @@ $(document).ready(function() {
 		$('#mybutton').hide();
 		var botID = "838585674:AAHc-oeJjg5FjJXI9X6vMbJS219rCtQgfRI";
 		var email = $("input[name='mail']").val()
-		console.log(email);
 		var text = $("textarea").val();
-		console.log(text);
-
 		var msg = "Rajab you have a Message from your website %0AEmail : "+email+" %0AMessage:  "+text;
 		// im gonna add my telegram url here
 		$.get("https://api.telegram.org/bot"+botID+"/sendMessage?chat_id=564221279&text="+msg+"", function(data, status){
@@ -58,9 +72,5 @@ function updateGradient(){
 	});
 	$('.pic_wrapper').css({
 		background: "linear-gradient("+angle+"deg, #2196f3  48%,#9932CC 77%)"
-	});
-	
-
-
-	
+	});	
 }
